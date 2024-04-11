f97d270bd3359aad4e1011a038e4604baeaeedab
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-04-11 11:13:56 +0200

    update

--- runnerz/src/main/java/dev/jadamathele/runnerz/run/RunRepository.java
+++ runnerz/src/main/java/dev/jadamathele/runnerz/run/RunRepository.java
@@ -1,4 +1,24 @@
 package dev.jadamathele.runnerz.run;
 
-public class RunRepository {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public interface RunRepository {
+
+    List<Run> findAll();
+
+    Optional<Run> findById(Integer id);
+
+    void create(Run run);
+
+    void update(Run run, Integer id);
+
+    void delete(Integer id);
+
+    int count();
+
+    void saveAll(List<Run> runs);
+
+    List<Run> findByLocation(String location);
 }
